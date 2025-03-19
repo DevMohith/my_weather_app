f193621a84b9a2b39c2a9ece43e3f872adfc7a4b
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-03-19 11:14:01 +0100

    Fixing Imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import 
 
 function App() {
   return (
